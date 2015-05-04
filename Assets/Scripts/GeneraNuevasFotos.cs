315d3cc47469d556062d8515d94394df4795ed1d
Alex <projectra@mta.udg.mx>
2015-05-04 06:42:09 -0500

    Version 0.1.0
    Replica
    ScrollRect
    Scripts

---
+++ Assets/Scripts/GeneraNuevasFotos.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GeneraNuevasFotos : MonoBehaviour {
+
+	public GameObject objetoBaseFoto1;
+	public GameObject objetoBaseFoto2;
+	public Sprite fx;
+	public Sprite xd;
+	public Image imagen2;
+
+	void Start () {
+	
+		imagen2=objetoBaseFoto2.GetComponent<Image> ();
+		imagen2.sprite = xd;
+	//	imagenes=objetoBaseFoto.GetComponent<Image> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
