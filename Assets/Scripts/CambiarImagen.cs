c648ab91df1568f4b0127dc0c1f089b01703516d
Alex <projectra@mta.udg.mx>
2015-05-07 07:49:45 -0500

    version 0.3

--- Assets/Scripts/CambiarImagen.cs
+++ Assets/Scripts/CambiarImagen.cs
@@ -7,49 +7,42 @@ using UnityEditor;
 public class CambiarImagen : MonoBehaviour {
 
 	public Image objetoImagen;
-	public Image[] brillo;
-	public Sprite amarillo;
-	public Sprite transparente;
-	public Sprite[] imagenes;
 	public int indice=0;
-	public int longitud;
-	public Image[] carrete;
 	public Scrollbar barra;
 
 	//-----------------------------
+	public Image fondo;
+	public Image thumb;
 	public Texture2D[] thumbs;
+	public Image panel;
 	//-----------------------------
+
 	// Use this for initialization
-	
 	void Start() {
-		objetoImagen.sprite = imagenes [0];
-
-		for (int i=0; i<=longitud; i++) {
-			brillo [i].sprite = transparente;
-			carrete [i].sprite = imagenes [i];
-		}
-			brillo [0].sprite = amarillo;
-
-		//-------------------------------------
 		thumbs = Resources.LoadAll<Texture2D>("Images");
-		for (int i=0; i<=longitud; i++) {
-			brillo [i].sprite = transparente;
-			brillo [0].sprite = amarillo;
-			Rect rec = new Rect (0, 0, thumbs [i].width, thumbs [i].height);
-			Vector2 vec = new Vector2 (0.5f, 0.5f);
-			carrete [i].sprite = Sprite.Create (thumbs [i], rec, vec);
+
+		Rect rec = new Rect (0, 0, thumbs [0].width, thumbs [0].height);
+		Vector2 vec = new Vector2 (0.5f, 0.5f);
+		thumb.sprite = Sprite.Create (thumbs [0], rec, vec);
+		objetoImagen.sprite = Sprite.Create (thumbs [0], rec, vec);
+
+		Image[] images = new Image[thumbs.Length];
+		for (int x=1; x<thumbs.Length; x++) {
+			images[x] = Instantiate (thumb);
+			rec = new Rect (0, 0, thumbs [x].width, thumbs [x].height);
+			images[x].sprite = Sprite.Create (thumbs [x], rec, vec);
+			images[x].transform.position = new Vector2 (thumb.transform.position.x + (66*x), thumb.transform.position.y);
+			images[x].transform.SetParent(panel.transform);
 		}
 
-		EditorUtility.DisplayDialog(thumbs.Length.ToString(), "Seguro que deseas continuar?", "Si", "No");
-		//-------------------------------------
-		
+		//EditorUtility.DisplayDialog(thumbs.Length.ToString(), "Seguro que deseas continuar?", "Si", "No");
 	}
+
 	// Update is called once per frame
 	public void Avanza () {
-		brillo [indice].sprite = transparente;
-		if (indice < longitud) {
+		if (indice < thumbs.Length) {
+			fondo.transform.position = new Vector2 (fondo.transform.position.x+66, fondo.transform.position.y);
 			indice++;
-			//objPan.position = new Vector2(objPan.position.x + 55,objPan.position.y);
 		}
 		if(indice == 6){
 			barra.value =  0.70248f;
@@ -58,19 +51,15 @@ public class CambiarImagen : MonoBehaviour {
 			barra.value =  1f;
 		}
 
-		brillo [indice].sprite = amarillo;
-		//-------------------------------------
 		Rect rec = new Rect (0, 0, thumbs [indice].width, thumbs [indice].height);
 		Vector2 vec = new Vector2 (0.5f, 0.5f);
-		objetoImagen.sprite = Sprite.Create (thumbs [indice], rec, vec); 
-		//-------------------------------------
+		objetoImagen.sprite = Sprite.Create (thumbs [indice], rec, vec);
 	}
 	
 	public void Retrocede() {
-		brillo [indice].sprite = transparente;
 		if (indice > 0) {
+			fondo.transform.position = new Vector2 (fondo.transform.position.x-66, fondo.transform.position.y);
 			indice--;
-			//objPan.position = new Vector2(objPan.position.x-55,objPan.position.y);
 		}
 		if(indice == 6){
 			barra.value =  0f;
@@ -78,12 +67,10 @@ public class CambiarImagen : MonoBehaviour {
 		if(indice == 12 ){
 			barra.value =  .70248f;
 		}
-		brillo [indice].sprite = amarillo;
-		//-------------------------------------
+
 		Rect rec = new Rect (0, 0, thumbs [indice].width, thumbs [indice].height);
 		Vector2 vec = new Vector2 (0.5f, 0.5f);
 		objetoImagen.sprite = Sprite.Create (thumbs [indice], rec, vec); 
-		//-------------------------------------
 
 	}
 }
