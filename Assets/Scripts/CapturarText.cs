c0faac32a4388386069df8dfecb66a9ed5329491
Emmanuel <emmanuel.aame@gmail.com>
2015-05-13 19:09:40 -0500

    Version 3.3.2

--- Assets/Scripts/CapturarText.cs
+++ Assets/Scripts/CapturarText.cs
@@ -4,15 +4,27 @@ using System.IO;
 using UnityEngine.UI;
 
 public class CapturarText : MonoBehaviour {
-	public string TextoAGuardar;
+	public string[] TextoAGuardar;
 	public InputField CampoTexto;
+
 	public void ActivarTexto (){
 		CampoTexto.gameObject.SetActive (true);
 	}
+	void Start(){
+		TextoAGuardar = new string[50];
+	}
 	public void guardar(){
-		TextoAGuardar = CampoTexto.text;
-		System.IO.File.WriteAllText ("./Resources/Fase1/Comentarios", TextoAGuardar);
-		//Debug.
+		string ruta;
+		ruta = Application.dataPath;
+		ruta += "/Resources/Comentarios/Comentario.txt";
+		System.IO.File.WriteAllLines (ruta, TextoAGuardar, System.Text.Encoding.UTF8);
 
+	}
+	void Update(){
+		int i = 0;
+		do{
+			TextoAGuardar[i] = CampoTexto.text;
+			i++;
+		}while(Input.GetKeyDown ("intro"));
 	}
 }
