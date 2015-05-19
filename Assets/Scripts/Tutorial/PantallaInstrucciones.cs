d900101d2e2e37a920f750a2a7c56a0d6204b40b
Emmanuel <emmanuel.aame@gmail.com>
2015-05-19 17:31:54 -0500

    Version 3.3.3

--- Assets/Scripts/Tutorial/PantallaInstrucciones.cs
+++ Assets/Scripts/Tutorial/PantallaInstrucciones.cs
@@ -1,14 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using System.IO;
 
 public class PantallaInstrucciones : MonoBehaviour {
 
 	public GameObject panelInstrucciones;
-
+	public Text texto;
+	public string ruta;
+	public string archivo;
 	// Use this for initialization
 	void Start () {
+		if (Application.loadedLevelName == "Fase1") {
+			archivo = "Fase1.txt";
+		}
+		if (Application.loadedLevelName == "Fase2") {
+			archivo = "Fase2.txt";
+		}
 		panelInstrucciones.SetActive (true);
-
+		ruta = "./Assets/Tutorial/"; 
+		ruta += archivo;
+		texto.text = File.ReadAllText (ruta);
 	}
 	public void boton(){
 		panelInstrucciones.SetActive (false);
